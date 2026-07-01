87f6ce27b0c229c5caa3bfc9c5652dbec574158f
0xPhantasia <evan.mestre@sanofi.com>
2026-07-01 10:23:46 +0200

    fix: remove unsupported arguments

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,9 @@ module "buckets" {
 }
 
 module "lambdas" {
-  source                 = "./modules/lambdas"
-  source_bucket_id       = module.buckets.source_bucket_id
-  source_bucket_arn      = module.buckets.source_bucket_arn
-  destination_bucket_id  = module.buckets.destination_bucket_id
-  destination_bucket_arn = module.buckets.destination_bucket_arn
+  source = "./modules/lambdas"
+#  source_bucket_id  = module.buckets.source_bucket_id
+  source_bucket_arn = module.buckets.source_bucket_arn
+#  destination_bucket_id    = module.buckets.destination_bucket_id
+  destination_bucket_arn   = module.buckets.destination_bucket_arn
 }
