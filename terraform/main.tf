cb5184d6a49bdc7ecedf8ff7f5387ff467c4d09e
0xPhantasia <evan.mestre@sanofi.com>
2026-07-01 10:25:46 +0200

    chore: linting terraform files, again

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ module "buckets" {
 
 module "lambdas" {
   source = "./modules/lambdas"
-#  source_bucket_id  = module.buckets.source_bucket_id
+  #  source_bucket_id  = module.buckets.source_bucket_id
   source_bucket_arn = module.buckets.source_bucket_arn
-#  destination_bucket_id    = module.buckets.destination_bucket_id
-  destination_bucket_arn   = module.buckets.destination_bucket_arn
+  #  destination_bucket_id    = module.buckets.destination_bucket_id
+  destination_bucket_arn = module.buckets.destination_bucket_arn
 }
